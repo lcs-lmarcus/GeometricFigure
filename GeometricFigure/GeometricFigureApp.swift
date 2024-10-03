8783f4c90023f4fde908f72bff0994f1ebc32f51
Marcus Li <mmli@lcs.on.ca>
2024-10-03 09:00:33 -0400

    Initial Commit

---
+++ GeometricFigure/GeometricFigureApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFigureApp.swift
+//  GeometricFigure
+//
+//  Created by Marcus Li on 2024-10-03.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFigureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
